de0aa2ef9b53d38c0ef82be15adf660fee2b3476
autsadawut.kl <autsadawut.dew10@gmail.com>
2024-04-05 13:25:10 +0700

    add font style

--- react-task-project/src/components/Header.js
+++ react-task-project/src/components/Header.js
@@ -1,4 +1,6 @@
 import "./Header.css";
+import { CiCloudSun } from "react-icons/ci";
+import { FaCloudMoon } from "react-icons/fa6";
 
 export default function Header(props) {
     const {theme, setTheme} = props;
@@ -16,7 +18,7 @@ export default function Header(props) {
             </div>
             <div className="theme-container">
                 <span>{theme === "light" ? "โหมดกลางวัน" : "โหมดกลางคืน"}</span>
-                <span className="icon" onClick={ToggleTheme}>สลับ</span>
+                <span className="icon" onClick={ToggleTheme}>{theme === "light" ? <CiCloudSun/> : <FaCloudMoon/>}</span>
             </div>
         </header>
     );
--- react-task-project/src/components/Item.js
+++ react-task-project/src/components/Item.js
@@ -1,14 +1,15 @@
 import "./Item.css";
+import { BiTrash,BiEdit } from "react-icons/bi";
 
 export default function Item(props) {
-    const {data, deleteTask, editTask} = props;
-    return (
-        <div className="list-item">
-            <p className="title">{data.title}</p>
-            <div className="button-container">
-                <button className="btn" onClick={()=>deleteTask(data.id)}>ลบ</button>
-                <button className="btn" onClick={()=>editTask(data.id)}>แก้ไข</button>
-            </div>
-        </div>
-    );
+  const { data, deleteTask, editTask } = props;
+  return (
+    <div className="list-item">
+      <p className="title">{data.title}</p>
+      <div className="button-container">
+        <BiTrash className="btn" onClick={() => deleteTask(data.id)} />
+        <BiEdit className="btn" onClick={() => editTask(data.id)}/>
+      </div>
+    </div>
+  );
 }
