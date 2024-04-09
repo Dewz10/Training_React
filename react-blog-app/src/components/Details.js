1724285540bc9c41a5917a70afd94c76e866ca2e
autsadawut.kl <autsadawut.dew10@gmail.com>
2024-04-09 09:58:06 +0700

    display content

--- react-blog-app/src/components/Details.js
+++ react-blog-app/src/components/Details.js
@@ -14,10 +14,16 @@ export default function Details() {
     setImage(result.image_url);
     setContent(result.content);
     setAuthor(result.author);
+    // eslint-disable-next-line
   },[])
   return (
-    <>
-    <h2>รายละเอียดบทความ: {id}</h2>
-    </>
+    <div className="container">
+      <h2 className="title">บทความ: {title}</h2>
+      <img src={image} alt={title} className="blog-image"/>
+      <div className="blog-detail">
+        <strong>ผู้เขียน: {author}</strong>
+        <p>{content}</p>
+      </div>
+    </div>
   );
 }
