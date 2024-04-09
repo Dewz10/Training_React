c221a02117f8c028ce61da7bd86840bf2efab36d
autsadawut.kl <autsadawut.dew10@gmail.com>
2024-04-09 09:43:43 +0700

    Update Details.js

--- react-blog-app/src/components/Details.js
+++ react-blog-app/src/components/Details.js
@@ -1,7 +1,12 @@
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import blogs from "../data/blogs";
 
 export default function Details() {
   const {id} = useParams();
+  useEffect(()=>{
+    const result = blogs.find((item)=>item.id === parseInt(id));
+  },[])
   return (
     <>
     <h2>รายละเอียดบทความ: {id}</h2>
