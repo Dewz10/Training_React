5f6c0a4941f14fb30ca8f830c95f42bd1a324097
autsadawut.kl <autsadawut.dew10@gmail.com>
2024-04-09 09:50:41 +0700

    set state

--- react-blog-app/src/components/Details.js
+++ react-blog-app/src/components/Details.js
@@ -1,11 +1,19 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import blogs from "../data/blogs";
 
 export default function Details() {
   const {id} = useParams();
+  const [title,setTitle] = useState("");
+  const [image,setImage] = useState("");
+  const [content,setContent] = useState("");
+  const [author,setAuthor] = useState("");
   useEffect(()=>{
     const result = blogs.find((item)=>item.id === parseInt(id));
+    setTitle(result.title);
+    setImage(result.image_url);
+    setContent(result.content);
+    setAuthor(result.author);
   },[])
   return (
     <>
