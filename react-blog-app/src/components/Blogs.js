4aa67fdbda1e76d876e26ba5ee3cbaa9a1542805
autsadawut.kl <autsadawut.dew10@gmail.com>
2024-04-09 10:13:54 +0700

    search data

--- react-blog-app/src/components/Blogs.js
+++ react-blog-app/src/components/Blogs.js
@@ -1,12 +1,29 @@
 import blogs from "../data/blogs";
 import { Link } from "react-router-dom";
 import "./Blogs.css";
+import { useState, useEffect } from "react";
 
 export default function Blogs() {
+  const [search, setSearch] = useState("");
+  const [filterBlogs, setFilterBlogs] = useState([]);
+  useEffect(() => {
+    const result = blogs.filter((item) => item.title.includes(search));
+    setFilterBlogs(result);
+    // eslint-disable-next-line
+  }, [search]);
   return (
     <div className="blog-container">
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="ค้นหาบทความ"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
       <article>
-        {blogs.map((item) => (
+        {filterBlogs.map((item) => (
           <Link to={`/blog/${item.id}`} key={item.id}>
             <div className="card">
               <h2>{item.title}</h2>
