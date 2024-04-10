b3a67420edf91e5dba5489bb75aa8a4a4dbd0691
autsadawut.kl <autsadawut.dew10@gmail.com>
2024-04-10 14:47:46 +0700

    Update App.jsx

--- search-image-app/src/App.jsx
+++ search-image-app/src/App.jsx
@@ -1,18 +1,32 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [word, setWord] = useState("");
 
+  function searchImage(e) {
+    e.preventDefault();
+    if (!word) {
+      alert("กรุณาป้อนข้อมูล");
+    } else {
+      // Call API
+      console.log(word);
+    }
+  }
   return (
     <>
       <h1>ระบบค้นหารูปภาพด้วย API</h1>
-      <form>
-        <input type="text" placeholder="รูปภาพที่ต้องการค้นหา"/>
+      <form onSubmit={searchImage}>
+        <input
+          type="text"
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+          placeholder="รูปภาพที่ต้องการค้นหา"
+        />
         <button type="submit">ค้นหา</button>
       </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
