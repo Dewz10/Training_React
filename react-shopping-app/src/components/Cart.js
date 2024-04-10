9cf3d84152ccefe7347240590bae8cfde15e6add
autsadawut.kl <autsadawut.dew10@gmail.com>
2024-04-10 09:43:51 +0700

    Update Cart.js

--- react-shopping-app/src/components/Cart.js
+++ react-shopping-app/src/components/Cart.js
@@ -2,9 +2,10 @@ import Item from "./Item";
 import { useCart } from "../context/CartContext"
 
 export default function Cart() {
-    const {products} = useCart()
+    const {products,total,formatMoney} = useCart()
     return (
         <div className="cart">
+            <h1 style={{textAlign:"center"}}>ยอดชำระเงินรวม : {formatMoney(total)} บาท</h1>
             {products.map((data)=>{
                 return <Item key={data.id} {...data}/>
             })}
